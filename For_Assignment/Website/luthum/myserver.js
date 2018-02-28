15edc53f69331699c4033d7f1b4b7f9c28ba460d
pangchunho1941 <pangchunho1941@gmail.com>
2018-02-28 07:50:39 -0500

    SignUp.html

---
+++ For_Assignment/Website/luthum/myserver.js
@@ -0,0 +1,63 @@
+var http = require('http');
+var fs = require("fs");
+
+http.createServer(function(request, response) {
+
+	if(request.url === "/index"){
+		//sendFileContent(response, "callajax.html", "text/html");
+		sendFileContent(response, "index.html", "text/html");
+	}
+	else if(request.url === "/"){
+		console.log("Requested URL is url" +request.url);
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write('<b>Hey there!</b><br /><br />This is the default response. Requested URL is: ' + request.url);
+	}
+	else if(/^\/[a-zA-Z0-9\/]*.js$/.test(request.url.toString())){
+		sendFileContent(response, request.url.toString().substring(1), "text/javascript");
+	}else if(/^\/[a-zA-Z0-9\/]*.min.js$/.test(request.url.toString())){
+		sendFileContent(response, request.url.toString().substring(1), "text/javascript");
+	}else if(/^\/[a-zA-Z0-9\/]*.mobilemenu.js$/.test(request.url.toString())){
+		sendFileContent(response, request.url.toString().substring(1), "text/javascript");
+	}else if(/^\/[a-zA-Z0-9\/]*.backtotop.js$/.test(request.url.toString())){
+		sendFileContent(response, request.url.toString().substring(1), "text/javascript");
+	}
+	
+	
+	
+	
+	
+	
+	
+	else if(/^\/[a-zA-Z0-9\/]*.css$/.test(request.url.toString())){
+		sendFileContent(response, request.url.toString().substring(1), "text/css");
+	}
+	else if(/^\/[a-zA-Z0-9\/]*.jpg$/.test(request.url.toString())){
+		sendFileContent(response, request.url.toString().substring(1), "image/jpg");
+			}
+	else if(/^\/[a-zA-Z0-9\/]*.png$/.test(request.url.toString())){
+		sendFileContent(response, request.url.toString().substring(1), "image/png");
+		}
+	else if(/^\/[a-zA-Z0-9\/]*.60X60.png$/.test(request.url.toString())){
+		sendFileContent(response, request.url.toString().substring(1), "image/png");
+		
+		
+	}else{
+		console.log("Requested URL is: " + request.url);
+		response.end();
+	}
+}).listen(9999)
+
+function sendFileContent(response, fileName, contentType){
+	console.log("Get: " + fileName);
+	fs.readFile(fileName, function(err, data){
+		if(err){
+			response.writeHead(404);
+			response.write("Not Found!");
+		}
+		else{
+			response.writeHead(200, {'Content-Type': contentType});
+			response.write(data);
+		}
+		response.end();
+	});
+}
